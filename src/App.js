8fcd44800c1e842faaed463d5e82fe13316a1f57
imdhruv99 <dpdhruvprajapati@gmail.com>
2021-08-31 17:16:50 +0530

    [ADDED] Charts, Completed the App

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ import Expenses from './components/Expenses/Expenses';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
+    title: 'Lunch',
+    amount: 100,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { 
+    id: 'e2', 
+    title: 'Tea and Snacks', 
+    amount: 80, 
+    date: new Date(2021, 2, 12) },
   {
     id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
+    title: 'Dinner',
+    amount: 294,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
+    title: 'Breakfast',
+    amount: 45 ,
     date: new Date(2021, 5, 12),
   },
 ];
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,7 +26,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -69,6 +69,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
